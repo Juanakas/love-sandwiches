b5d90f0d7c04f690bff167d41ebc7e4cc0fcf6c8
Juanakas <juanclementeguerrero@outlook.com>
2025-12-06 15:38:51 +0000

    Add validation to check for exactly 6 values in data provided.

--- run.py
+++ run.py
@@ -13,7 +13,19 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
+    # The user inputs the data for the sales.
     data_str = input("Input sales data:")
-    print(f"Your data: {data_str}")
+    # The data is split into a list.
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    try:
+        if len(values) != 6: # The user must enter 6 values.
+            raise ValueError(f"There must be 6 values, you entered {len(values)} values.")
+    except ValueError as e: #e means error.
+        print(f"Invalid data: {e}, please try again.\n")
+        return
 
 get_sales_data()
